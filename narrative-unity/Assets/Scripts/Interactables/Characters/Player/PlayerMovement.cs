b80d5f41ea6b36e0a7aa02257827a450f242fa93
lilly <mail@frederyk.de>
2019-06-11 11:24:30 +0200

    Clean up PlayerMovement code and add documentation

--- narrative-unity/Assets/Scripts/Interactables/Characters/Player/PlayerMovement.cs
+++ narrative-unity/Assets/Scripts/Interactables/Characters/Player/PlayerMovement.cs
@@ -24,10 +24,10 @@ public class PlayerMovement : MonoBehaviour
     [Range(.1f, .5f)]
     float rotationSpeed = .14f;
 
+    // Set forward so the player doesn't start laying on the ground
     Vector3 lastMove = Vector3.forward;
 
     CharacterController controller;
-    CapsuleCollider capsuleCollider;
     Vector2 inputAxis;
 
     private void Start()
@@ -38,7 +38,6 @@ public class PlayerMovement : MonoBehaviour
     void GetAllComponents()
     {
         controller = GetComponent<CharacterController>();
-        capsuleCollider = GetComponent<CapsuleCollider>();
     }
 
     void Update()
@@ -46,13 +45,18 @@ public class PlayerMovement : MonoBehaviour
         DoPlayerMovement();
     }
 
+    /*
+     * Do all checks and calculations to move the player
+     */
     void DoPlayerMovement()
     {
+        // Don't allow player movement when the inventory is open
         if (GameManager.GLOBAL.inventoryManager.isOpen)
             return;
 
         GetInputAxis();
 
+        // Only proceed further if there is any input
         bool movementInput = inputAxis.magnitude > float.Epsilon;
 
         if (movementInput)
@@ -70,42 +74,42 @@ public class PlayerMovement : MonoBehaviour
         isRunning = Input.GetAxisRaw("Run") > float.Epsilon;
     }
 
+    /*
+     * Calculate desired movement and physics and send it to the character controller
+     */
     void Move()
     {
+        // Calculate desired movement from input and forward direction
         Vector3 desiredForwardMotion = cameraArm.transform.forward * inputAxis.y;
         Vector3 desiredSidewardMotion = cameraArm.transform.right * inputAxis.x;
 
         Vector3 desiredMove = desiredForwardMotion + desiredSidewardMotion;
 
-        /*
-         * Toggle walking/running speed
-         */
+        // Toggle walking/running speed
         movementSpeed = isRunning ? runningSpeed : walkingSpeed;
 
-        /*
-         * Force movement on XZ plane, introduce movement speed and apply gravity
-         * since we are not using SimpleMove()
-         */
+        // Force movement on XZ plane, introduce movement speed and apply gravity
         desiredMove = Vector3.ProjectOnPlane(desiredMove, Vector3.up).normalized;
         desiredMove *= movementSpeed;
         desiredMove += (Physics.gravity * Time.deltaTime);
 
-        /*
-         * Update class variable for rotation later on
-         */
+        // Update class variable for rotation later on
         lastMove = desiredMove * Time.deltaTime;
 
-        /*
-         * Let the controller handle movement for better collision control
-         */
+        // Let the controller handle movement for better collision control
         controller.Move(lastMove);
     }
 
+    /*
+     * Lerp the player facing towards the last movement direction
+     */
     void RotatePlayerModel()
     {
+        // Get last mmovement direction and set Y to 0 to prevent tilt
         Vector3 viewingRotation = lastMove.normalized;
         viewingRotation.y = 0f;
 
+        // Lerp forward vector to prevent sudden value changes
         playerModel.transform.forward = Vector3.Lerp
         (
             playerModel.transform.forward,
