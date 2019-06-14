5f5149198c87f54d2ca496f579ca35f1e9ffdb82
lilly <mail@frederyk.de>
2019-06-14 16:47:20 +0200

    Move test scenes into folder, add xylophone puzzle, add Main Scene

---
+++ narrative-unity/Assets/Scripts/Interactables/Interactables/Interactable_XylophoneStick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable_XylophoneStick : Interactable
+{
+    public override void Interact()
+    {
+        base.Interact();
+
+        EventManager.Global.TriggerEvent("Puzzle_Xylophone", this.gameObject);
+    }
+}
