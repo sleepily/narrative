fe2110ebf228077baa5d5d741335dae7964114d3
lilly <mail@frederyk.de>
2019-06-24 10:27:14 +0200

    Split the main scene

---
+++ narrative-unity/Assets/Scripts/Tools/CanvasPlayerCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasPlayerCamera : MonoBehaviour
+{
+    Canvas canvas;
+
+    private void Start()
+    {
+        canvas = GetComponent<Canvas>();
+        canvas.worldCamera = GameManager.GLOBAL.player.thirdPersonCamera;
+    }
+}
---
+++ narrative-unity/Assets/Scripts/Tools/TransportPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransportPlayer : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        CheckCollision(collision);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        CheckCollision(collision);
+    }
+
+    void CheckCollision(Collision collision)
+    {
+        Player player = collision.collider.GetComponent<Player>();
+
+        if (!player)
+            return;
+
+
+    }
+}
