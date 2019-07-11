e67ef5cfdb03e14edd3b7cabf7b45d41ecde98bb
lilly <mail@frederyk.de>
2019-07-11 11:33:10 +0200

    Add fireflies, apply some UVs, change material folder structure

---
+++ narrative-unity/Assets/Fireflies.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ex = Tools.ExtensionMethods;
+
+public class Fireflies : MonoBehaviour
+{
+    public Firefly firefly;
+
+    [Header("Spawn and Movement")]
+    [Range(1, 50)]
+    public int amount = 30;
+    [Range(.1f, 5f)]
+    public float spawnRadius = 2f;
+    [Range(.1f, 1f)]
+    public float avoidanceDistance = .4f;
+    [Range(4, 200)]
+    public float rotationDamping = 120f;
+    public bool applyJitter = true;
+    [Range(0f, 2f)]
+    public float jitterStrength = .2f;
+
+    [Header("Firefly Properties")]
+    [Tooltip("Minimum and maximum firefly speed.")]
+    public Vector2 size = new Vector2(0.02f, .08f);
+    public Vector2 speed = new Vector2(0.15f, 1.5f);
+    public Vector2 intensity = new Vector2(0.01f, 0.14f);
+
+    [Space]
+
+    [Tooltip("Scale the speed with size.")]
+    public bool speedRelativeToSize = true;
+
+    [Tooltip("Scale the light intensity with speed.")]
+    public bool intensityRelativeToSpeed = true;
+
+    [HideInInspector]
+    public Firefly[] fireflies { get; private set; }
+
+    private void Start()
+    {
+        SetupFireflies();
+        StartGlowing();
+    }
+
+    void SetupFireflies()
+    {
+        if (amount < 1)
+            amount = 1;
+
+        fireflies = new Firefly[amount];
+
+        for (int i = 0; i < fireflies.Length; i++)
+        {
+            fireflies[i] = Instantiate(firefly).SetFireflies(this);
+            fireflies[i].transform.localPosition += Random.insideUnitSphere * spawnRadius;
+
+            fireflies[i].size = Random.Range(size.x, size.y);
+            fireflies[i].speed = speedRelativeToSize
+                ? Ex.Map(fireflies[i].size, size.x, size.y, speed.y, speed.x)
+                : Random.Range(speed.x, speed.y);
+            fireflies[i].intensity = intensityRelativeToSpeed
+                ? Ex.Map(fireflies[i].speed, speed.x, speed.y, intensity.x, intensity.y)
+                : Random.Range(intensity.x, intensity.y);
+        }
+    }
+
+    public void StartGlowing()
+    {
+        if (fireflies == null || fireflies.Length == 0)
+            SetupFireflies();
+
+        for (int i = 0; i < fireflies.Length; i++)
+            fireflies[i].StartMoving();
+    }
+
+    public void StopGlowing()
+    {
+        if (fireflies == null || fireflies.Length == 0)
+            SetupFireflies();
+
+        for (int i = 0; i < fireflies.Length; i++)
+            fireflies[i].StopMoving();
+    }
+}
---
+++ narrative-unity/Assets/Firefly.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ex = Tools.ExtensionMethods;
+
+public class Firefly : MonoBehaviour
+{
+    public float size, speed, intensity;
+    public bool hasJitter = true;
+
+    bool canMove = false;
+
+    Fireflies fireflies;
+    Material material;
+
+    private void Awake()
+    {
+        material = GetComponent<MeshRenderer>().material;
+        StopMoving();
+    }
+
+    public Firefly SetFireflies(Fireflies fireflies)
+    {
+        this.fireflies = fireflies;
+        transform.parent = fireflies.transform;
+        transform.localPosition = Vector3.zero;
+        return this;
+    }
+
+    public void StartMoving()
+    {
+        transform.localScale = Vector3.one * size;
+
+        Color emissionColor = material.GetColor("_EmissionColor");
+        emissionColor *= intensity;
+        material.SetColor("_EmissionColor", emissionColor);
+
+        canMove = true;
+    }
+
+    public void StopMoving()
+    {
+        canMove = false;
+
+        transform.localScale = Vector3.zero;
+    }
+
+    private void Update() => Move();
+
+    void Move()
+    {
+        if (!canMove)
+            return;
+
+        RotateByFlockingRules();
+        Vector3 jitter = ApplyJitter(speed * intensity, speed * size);
+
+        transform.Translate(jitter + Vector3.forward * Time.deltaTime * speed);
+    }
+
+    void RotateByFlockingRules()
+    {
+        Vector3 avoidanceVector = Vector3.zero;
+
+        Vector3 centerPosition = transform.parent.position;
+
+        foreach (Firefly firefly in fireflies.fireflies)
+        {
+            if (firefly == this)
+                continue;
+
+            // add fish vector to center position (if not following mouse)
+            centerPosition += firefly.transform.position;
+
+            bool fireflyIsNearby = Vector3.Distance(firefly.transform.position, this.transform.position) <= fireflies.avoidanceDistance;
+
+            if (fireflyIsNearby)
+                avoidanceVector += this.transform.position - firefly.transform.position;
+        }
+
+        if (fireflies.fireflies.Length != 0)
+            centerPosition /= fireflies.fireflies.Length;
+
+        Vector3 newDirection = (centerPosition + avoidanceVector) - this.transform.position;
+
+        transform.rotation =
+            Quaternion.Slerp
+            (
+                this.transform.rotation,
+                Quaternion.LookRotation(newDirection),
+                Time.deltaTime / (fireflies.rotationDamping * size)
+            );
+    }
+
+    Vector3 ApplyJitter(float multiplierX, float multiplierY)
+    {
+        if (!hasJitter)
+            return Vector3.zero;
+
+        float noiseX = Mathf.PerlinNoise(Time.time * multiplierX, Time.time * multiplierY);
+        float noiseY = Mathf.PerlinNoise(Time.time * multiplierY, Time.time * multiplierX);
+
+        float noiseZ = Random.Range(noiseX, noiseY);
+
+        float jitterScale = Ex.Map(speed, fireflies.speed.x, fireflies.speed.y, 0, fireflies.jitterStrength);
+
+        Vector3 jitter = new Vector3(noiseX, noiseY, noiseZ).normalized;
+        jitter -= Vector3.one * .5f;
+        jitter *= jitterScale;
+
+        return jitter * Time.deltaTime;
+    }
+}
